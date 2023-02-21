0ea6934b7dacce43aae285bd72c9e8bb3e92c1df
James Yong <james@konvoykegs.com>
2023-02-22 10:54:22 +1100

    feat: added MarshalJSON

--- internal/entity/keg.go
+++ internal/entity/keg.go
@@ -1,5 +1,11 @@
 package entity
 
+import "time"
+
+type Date struct {
+	time.Time
+}
+
 type Keg struct {
 	Id       string  `json:"id"`
 	DeviceId *string `json:"deviceId"`
@@ -9,4 +15,10 @@ type Keg struct {
 		Spear       string `json:"spear"`
 		Description string `json:"description"`
 	} `json:"sku"`
+	Manufacturer     string `json:"manufacturer"`
+	ManufacturerDate Date   `json:"manufacturerDate"`
+}
+
+func (d *Date) MarshalJSON() ([]byte, error) {
+	return []byte(d.Format("2006-01-02")), nil
 }
